b87058ec784241441141c110b5e866dc0c49dc6e
rakesh1268248 <114190126+rakesh1268248@users.noreply.github.com>
2022-10-18 09:50:20 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import pydaisi as pyd
 
-greeting_daisi = pyd.Daisi("feedexpedition/pdf_extraction")
-greeting_daisi.pdf_extract().value
+greeting_daisi = pyd.Daisi("exampledaisies/Print Hello")
+greeting_daisi.hello(name = "Daisi user").value
